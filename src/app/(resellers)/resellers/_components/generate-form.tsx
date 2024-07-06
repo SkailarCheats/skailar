00e653eb03f370af0e60bae17cd9a66539ae4fd8
Edo <hello@edoardo-hb.it>
2024-07-06 19:52:22 +0200

    push

--- src/app/(resellers)/resellers/_components/generate-form.tsx
+++ src/app/(resellers)/resellers/_components/generate-form.tsx
@@ -6,7 +6,7 @@ import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { User } from "@/payload-types";
 import { useRouter } from "next/navigation";
-import { useState, useEffect, FormEvent, ChangeEvent } from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import { toast } from "sonner";
 
 interface Balance {
