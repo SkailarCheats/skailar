51942f4d45c06e3e47957fa26816e068ce750967
Edo <hello@edoardo-hb.it>
2024-07-22 00:21:17 +0200

    feat: disabled checkouts

--- src/app/(root)/cart/page.tsx
+++ src/app/(root)/cart/page.tsx
@@ -309,7 +309,7 @@ const Page = () => {
                             <Button
                                 className="w-full"
                                 size="lg"
-                                disabled={items.length === 0}
+                                disabled={items.length === 0 || true}  // TODO: Remove true
                                 onClick={() => setOpen(true)}
                             >
                                 Checkout
